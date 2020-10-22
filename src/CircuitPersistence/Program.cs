1ff619e380de752c0455236ddce9663af4b8e949
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2020-10-22 10:49:13 +0200

    add some logging infrastructure

--- src/CircuitPersistence/Program.cs
+++ src/CircuitPersistence/Program.cs
@@ -1,23 +1,42 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace CircuitPersistence
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+               var host = CreateHostBuilder(args).Build();
+               
+               await host.RunAsync();
+               return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "A fatal error caused service to crash");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, configuration) =>
+				{
+					configuration
+						.Enrich.FromLogContext()
+						.Enrich.WithProperty("Application", "CircuitPersistenceSample")
+						.ReadFrom.Configuration(context.Configuration);
+				})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
